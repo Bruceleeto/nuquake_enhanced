4da3ac5fb90246a59e1be6ec7facf69ee6deb158
bruceleeto <astevens092a@gmail.com>
2025-05-24 09:27:56 +0100

    update libgldc and add kos_fs.h

--- src/dreamcast/sys_dreamcast.c
+++ src/dreamcast/sys_dreamcast.c
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <malloc.h>
 
 #include <dc/sq.h>
-
+#include <kos/fs.h>
 #include "errno.h"
 /* Not using LZO1X right now */
 //#include "minilzo.h"
